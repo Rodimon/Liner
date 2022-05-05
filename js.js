9da7ae15d4b826cf50576db3d82555b719d7908b
Rodimon <102424756+Rodimon@users.noreply.github.com>
2022-05-05 20:26:26 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,44 @@
+let canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    count = 0
+canvas.width = 0
+canvas.height = 0
+function loop2() {
+    let input = document.getElementById("number"),
+        button = document.getElementById("button"),
+        value = input.value
+    input.remove()
+    button.remove()
+    grid = +value,
+        width = grid,
+        height = grid,
+        x = -grid,
+        y = -grid,
+        dy = grid,
+        dx = grid
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    requestAnimationFrame(loop);
+}
+function loop() {
+    requestAnimationFrame(loop);
+    if (++count < 1) {
+        return
+    }
+    count = 0
+    x += dx;
+    y += dy;
+    if (x >= canvas.width - width) {
+        dx = -grid
+    }
+    if (y >= canvas.height - height) {
+        dy = -grid
+    }
+    if (y <= 0) {
+        dy = grid
+    }
+    if (x <= 0) {
+        dx = grid
+    }
+    context.fillRect(x, y, width, height);
+}
